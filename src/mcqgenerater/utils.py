06326c87aa9617a1ae8d39f2b3d18661821ac1ae
adil <madilnaeem0@gmail.com>
2024-07-07 15:08:03 +0500

    this file typically serves as a collection of utility functions or helper functions that are used throughout the codebase

--- src/mcqgenerater/utils.py
+++ src/mcqgenerater/utils.py
@@ -2,49 +2,41 @@ import os
 import PyPDF2
 import json
 import traceback
-
+from src.mcqgenerater.logger import logger
+import pandas as pd
 
 def read_file(file):
-    if file.name.endswith(".pdf"):
-        try:
-            pdf_reader=PyPDF2.PdfFileReader(file)
+    try:
+        if file.name.endswith(".pdf"):
+            pdf_reader = PyPDF2.PdfFileReader(file)
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
             return text
-            
-        except Exception as e:
-            raise Exception("error reading the PDF file")
-        
-    elif file.name.endswith(".txt"):
-        return file.read().decode("utf-8")
-    
-    else:
-        raise Exception(
-            "unsupported file format only pdf and text file suppoted"
-            )
-
-def get_table_data(quiz_str):
-    try:
-        # convert the quiz from a str to dict
-        quiz_dict = json.loads(quiz_str)
-        quiz_table_data = []
-        
-        # iterate over the quiz dictionary and extract the required information
-        for key,value in quiz_dict.items():
-            mcq = value["mcq"]
-            options = " || ".join(
-                [
-                    f"{option}-> {option_value}" for option, option_value in value["options"].items()
-                 
-                 ]
-            )
-            
-            correct=value["correct"]
-            quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
-        
-        return quiz_table_data
-        
+        elif file.name.endswith(".txt"):
+            return file.read().decode("utf-8")
+        else:
+            raise Exception("Unsupported file format. Only PDF and text files are supported.")
     except Exception as e:
+        logger.error(f"Error reading file: {e}")
+        raise
+
+def get_table_data(quiz):
+    try:
+        logger.info(f"Parsing quiz data: {quiz}")
+        quiz_data = json.loads(quiz)
+
+        table_data = []
+        for q_num, q in quiz_data.items():
+            mcq = q["mcq"]
+            choices = " || ".join([f"{key}-> {choice}" for key, choice in q["options"].items()])
+            correct = q["correct"]
+            table_data.append({"MCQ": mcq, "Choices": choices, "Correct": correct})
+        
+        return pd.DataFrame(table_data)
+    except Exception as e:
+        logger.error(f"Error parsing quiz data: {e}")
         traceback.print_exception(type(e), e, e.__traceback__)
-        return False
+        return None
+
+
